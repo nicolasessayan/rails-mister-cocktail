3b1835e52d44bd8bf72fb8c25106cb3bd8d8e760
Nicolas Essayan <nicolas_essayan@yahoo.fr>
2019-02-14 16:29:46 +0100

    Solved

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+json_hash = JSON.parse(open(url).read)
+ingredients_attributes = []
+
+json_hash['drinks'].each do |hash|
+  ingredients_attributes << { name: hash['strIngredient1'] }
+end
+
+puts 'Cleaning ingredientss database...'
+Ingredient.destroy_all
+puts 'Creating ingredients...'
+
+Ingredient.create!(ingredients_attributes)
+puts 'Finished!'
